cc3e2dc64b491db989c2c2c75aa06ae7d0111ee7
unknown <adibcse10@gmail.com>
2021-11-02 16:00:09 +0600

    added footer

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from '../footer/Footer.module.css';
+
+const Footer = () => {
+    return (
+        <div className={styles.footer}>
+            <p>Copy right @GaziAdib</p>
+        </div>
+    )
+}
+
+export default Footer
